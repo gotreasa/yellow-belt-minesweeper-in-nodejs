2d505b1858c482c064cd25f57afe9065d506c51f
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-08-30 15:19:35 +0100

    feat: board has 10 columns

---
+++ src/Board.js
@@ -0,0 +1,9 @@
+class Board {
+  constructor() {
+    this.squares = Array(10);
+  }
+}
+
+module.exports = {
+  Board,
+};
--- src/Dummy.js
+++
@@ -1,9 +0,0 @@
-class Dummy {
-  constructor() {
-    this.valid = true;
-  }
-}
-
-module.exports = {
-  Dummy,
-};
