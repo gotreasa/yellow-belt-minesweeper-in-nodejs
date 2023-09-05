4f6f0bfa37b84d0bf124cc25c50bf0c9898aea74
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-09-05 18:15:43 +0100

    feat: handle exper user board

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ const { Board } = require('./Board');
 const SIZE = {
   beginner: [9, 9],
   intermediate: [16, 16],
+  expert: [16, 30],
 };
 
 class Game {
--- test/Game.test.js
+++ test/Game.test.js
@@ -1,5 +1,9 @@
 const { Game } = require('../src/Game');
-const { empty9x9Board, empty16x16Board } = require('./fixtures/emptyBoards');
+const {
+  empty9x9Board,
+  empty16x16Board,
+  empty30x16Board,
+} = require('./fixtures/emptyBoards');
 
 describe('Initialise the Game', () => {
   test('should set up a beginner user board', () => {
@@ -10,5 +14,10 @@ describe('Initialise the Game', () => {
   test('should set up an intermediate user board', () => {
     const game = new Game('intermediate');
     expect(game.userBoard.squares).toEqual(empty16x16Board);
+  });
+
+  test('should set up an expert user board', () => {
+    const game = new Game('expert');
+    expect(game.userBoard.squares).toEqual(empty30x16Board);
   });
 });
