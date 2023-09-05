8df896a5167e9086a2f275df972a7a0c849dece6
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-09-05 14:43:15 +0100

    feat: handle setting any combination of mines

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,12 @@ class Board {
   constructor(nuberOfColumns, numberOfRows) {
     this.squares = [...Array(numberOfRows)].map(() => Array(nuberOfColumns));
   }
+
+  setMines(minePositions) {
+    minePositions.forEach((minePosition) => {
+      this.squares[minePosition[0]][minePosition[1]] = '☢️';
+    });
+  }
 }
 
 module.exports = {
